f5fc84d6b5e5b710a6b1635cf296a3431b3518c4
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-31 16:56:50 +0100

    UI Integration arithmetical operations update the display test passing

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,13 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('arithmetical operations update the display with the result of the operation', () => {
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number5').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '10')
+  })
+
 })
