fca14b845ce66d885399bd098094d8e19557e343
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-31 15:51:57 +0100

    Initial files set up

---
+++ src/tests/Calculator.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Calculator from '../containers/Calculator';
+import {mount, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+describe('Calculator', () => {
+  let container;
+  beforeEach(() => {
+    container = mount(<Calculator/>)
+  })
+
+  it('should change running total on number enter', () => {
+    const button4 = container.find('#number4');
+    const runningTotal = container.find('#running-total');
+    button4.simulate('click');
+    expect(runningTotal.text()).toEqual('4');
+  })
+})
+
